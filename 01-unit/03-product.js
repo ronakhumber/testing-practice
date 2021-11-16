7357172d31737c92c8f69146f1b940d39f464f82
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-16 21:22:07 +0530

    Adding tests for multiply method

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -29,22 +29,27 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
-
+  var result = product(0, 12);
+  if (result !== 0) throw new Error(`Expected product(0,12) to be 0. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = product(8, 5, 3, 1, 7, 13);
+  if (result !== 40) throw new Error(`Expected product(8,5,3,1,7,13) to be 40. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(12);
+  if (result !== 12) throw new Error(`Expected product(12) to be 12. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = product();
+  if (result !== 0) throw new Error(`Expected product() to be 0. Received: ${result}`);
+  
   console.log('All tests passed successfully.');
 
 // ==================================================
