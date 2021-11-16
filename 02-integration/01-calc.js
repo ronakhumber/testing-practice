55655061680aa06bc1621c6cf1839ab989edae05
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-16 21:36:02 +0530

    Add test case for all calculations in part 2

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -74,7 +74,7 @@ function quotient(num1, num2) {
 // Function calc
 // Takes 3 params : calculation, first number, second number
 // returns the result after performing arithmetic operation
-function calc() {
+function calc(calculation, firstNumber, secondNumber) {
   // Using SWITCH cases to determine which calculation
   // user wants to perform & call the function accordingly.
   switch (calculation) {
@@ -100,22 +100,26 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
-
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error(`Expected calc("subtract", 20, 10) to be 10. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
-
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error(`Expected calc("multiply", 9, 9) to be 81. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error(`Expected calc("divide", 9, 3) to be 3. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
-
+  var result = calc('exponent', 2, 8);
+  if (result !== 'Operation not supported.') throw new Error(`Expected calc("exponent", 2, 8) to be 'Operation not supported.'. Received: ${result}`);
   console.log('All tests passed successfully.');
 
 // ==================================================
