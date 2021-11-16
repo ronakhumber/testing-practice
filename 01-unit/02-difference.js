515bbe4aef4ffe4abcd1a6f55ad517022f156865
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-16 21:19:39 +0530

    Adding tests for difference method

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -29,17 +29,21 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = difference(1, 3, 5, 7, 9);
+  if (result !== -2) throw new Error(`Expected difference(1,3,5,7,9) to be -2. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = difference(7);
+  if (result !== 7) throw new Error(`Expected difference(7) to be 7. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = difference();
+  if (result !== 0) throw new Error(`Expected difference() to be 0. Received: ${result}`);
+  
   console.log('All tests passed successfully.');
 
 // ==================================================
