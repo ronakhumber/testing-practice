057dcb2d4f3559155f22ee322a37746290fab65a
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-16 21:16:26 +0530

    Adding tests for Sum method

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -29,17 +29,21 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = sum(1, 1, 2, 2, 3, 3);
+  if (result !== 2) throw new Error(`Expected sum(1,1,2,2,3,3) to be 2. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(1);
+  if (result !== 1) throw new Error(`Expected sum(1) to be 1. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = sum();
+  if(result !== 0) throw new Error(`Expected sum() to be 0. Received: ${result}`);
+  
   console.log('All tests passed successfully.');
 
 // ==================================================
