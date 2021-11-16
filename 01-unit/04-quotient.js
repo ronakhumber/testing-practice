8d48e535a931310d14d987ca6d0f182290c8e39c
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-11-16 21:25:29 +0530

    Adding tests for quotient method

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -15,7 +15,7 @@ function quotient(num1, num2) {
     return "ERROR";
   }
   // Return the sum of both the numbers
-  return num1 * num2;
+  return num1 / num2;
 }
 
 // ==================================================
@@ -33,21 +33,26 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(15, 0);
+  if (result !== 'ERROR') throw new Error(`Expected quotient(15,0) to be ERROR. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = quotient(18, 3, 12, 4, 21);
+  if (result !== 6) throw new Error(`Expected quotient(18,3,12,4,21) to be 6. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = quotient(24);
+  if (result !== 24) throw new Error(`Expected quotient(24) to be 24. Received: ${{ result }}`);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error(`Expected quotient() to be 0. Expected: ${result}`);
 
   console.log('All tests passed successfully.');
 
